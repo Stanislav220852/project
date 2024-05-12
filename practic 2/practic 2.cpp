f677c549bc94f5f28415f7901fdea632fd8c2473
Stanislav <141738543+Stanislav220852@users.noreply.github.com>
2024-05-12 16:50:11 +0300

    Add files via upload

---
+++ practic 2/practic 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+	int size_sym = 0;
+	int user_size_sym;
+	int opt;
+	char user_sym;
+	cout << "Enter number: ";
+	cin >> user_sym;
+	cout << "Enter number of characters: ";
+	cin >> user_size_sym;
+	cout << "Enter option: \n" << "1.Vertical line \n" << "2.Horizontal line\n";
+	cin >> opt;
+	if (opt == 1) {
+		if (user_sym == user_sym) {
+			while (size_sym < user_size_sym) {
+				cout << user_sym;
+				size_sym++;
+			}
+		}
+		cout << "\n";
+	}
+	else if (opt == 2) {
+		if (user_sym == user_sym) {
+			while (size_sym < user_size_sym) {
+				cout << user_sym<<"\n";
+				size_sym++;
+			}
+		}
+		cout << "\n";
+	}
+
+	main();
+
+}
+
+	
+	
