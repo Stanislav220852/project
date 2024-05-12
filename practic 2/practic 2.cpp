88aca966ce7f3cb5f70bf895d50c059079253241
Stanislav220852 <141738543+Stanislav220852@users.noreply.github.com>
2024-05-12 16:10:37 +0300

    Add files via upload

---
+++ practic 2/practic 2.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int size_sym = 0;
+	int user_size_sym;
+	int opt;
+	char user_sym;
+	cout << "Введите символ: ";
+	cin >> user_sym;
+	cout << "Введите число символов: ";
+	cin >> user_size_sym;
+	cout << "Введите опцию: \n" << "1.Вертикальная линия\n" << "2.Горизонтальная линия\n";
+	cin >> opt;
+	if (opt == 1) {
+		if (user_sym == user_sym) {
+			while (size_sym < user_size_sym) {
+				cout << user_sym;
+				size_sym++;
+			}
+		}
+		cout << "\n";
+	}
+	else if (opt == 2) {
+		if (user_sym == user_sym) {
+			while (size_sym < user_size_sym) {
+				cout << user_sym<<"\n";
+				size_sym++;
+			}
+		}
+		cout << "\n";
+	}
+	main();
+
+}
+
+	
+	/*switch (sym)
+	{
+	case '!': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "! ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "! \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+
+	}
+			break;
+
+	case '@': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "@ ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "@ \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+	}
+			break;
+
+	case '#': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "# ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "# \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+	}
+			break;
+	case '$': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "$ ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "$ \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+	}
+			break;
+	case '%': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "% ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "% \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+	}
+			break;
+	case '^': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "^ ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "^ \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+	}
+			break;
+	case '&': {
+		if (opt == 1) {
+			while (size_sym < user_size_sym) {
+				cout << "& ";
+				size_sym += 1;
+			}
+		}
+		else if (opt == 2) {
+			while (size_sym < user_size_sym) {
+				cout << "& \n";
+				size_sym += 1;
+			}
+		}
+		else {
+			cout << "Error";
+		}
+		cout << "\n";
+		main();
+	}
+			break;
+	default: {
+		cout << "Error\n";
+	
+		main();
+	}
+		   break;
+	}*/
+
+
+
+	
